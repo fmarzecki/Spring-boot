9444a3c45a43ad31ff43b62fa6d49b493a752ed8
fmarzecki <filipo085@icloud.com>
2023-03-16 20:07:43 +0100

    Spring App

---
+++ FirstSection/src/main/java/com/udemycourse/FirstSection/FirstSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemycourse.FirstSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSectionApplication.class, args);
+	}
+
+}
